9d0ec4933f80d5e40f74816c038f181f023226f4
Dusty Williams <duaty.wil@gmail.com>
2018-11-08 19:21:17 -0500

    updates for story edit page - dmw

--- client/auth/src/components/general/AddEditStory.vue
+++ client/auth/src/components/general/AddEditStory.vue
@@ -9,74 +9,80 @@
                         {{story.title}}
                     </li>
                 </ul>
-            </div>
-            
-            <form class="editAttributeForm" id="editStoryForm">
-                <label class="editFormLbl" for="storyTitle">Story Title:</label>
-                <input type="text" class="editFormTxt" id="storyName" v-model="selectedStory.name" placeholder="Story Name"/>
-                
-                <label class="editFormLbl" for="storyDesc">Story Description:</label>
-                <textarea type="text" class="editFormTxt" id="storyDesc" v-model="selectedStory.description">Story Description</textarea>
-                
-                <label class="editFormLbl" for="storyAuthor">Story Author(s):</label>
-                <input type="text" class="editFormTxt" id="storyAuthor"  placeholder="Author Names"/>
-                
-                <label class="editFormLbl" for="storagePath">Upload Video:</label>
-                <input type="file" class="editFormTxt" id="storagePath"/>
-                
-                <label class="editFormLbl" for="youtubeId">YouTube ID:</label>
-                <input type="text" class="editFormTxt" id="youtubeId" placeholder="YouTube ID"/>
-                
-                <label class="editFormCheckLbl" for="youtubeId">Featured Rotation:</label>
-                <input type="checkbox" class="editFormCheck" id="featuredRotation" />
-
-                <input type="hidden" id="storyId" value=""/>
                 
                 <div class="formBtnContainer">               
                     <button class="formBtn saveBtn" v-on:click="saveStory">Save</button>
                     <button class="formBtn clearBtn" v-on:click="clearForm" type="reset">Cancel</button>
                     <button class="formBtn delBtn" v-on:click="delStory">Delete</button>
                 </div>
+            </div>
+            
+            <form class="editAttributeForm" id="editStoryForm">
+                <label class="editFormLbl" for="storyTitle">Story Title:</label>
+                <input type="text" class="editFormTxt" id="storyName" v-model="selectedStory.title" placeholder="Story Title"/>
+                
+                
+                <label class="editFormLbl" for="storyDesc">Story Description:</label>
+                <textarea type="text" class="editFormTxtArea" id="storyDesc" v-model="selectedStory.description">Story Description</textarea>
+                
+                <label class="editFormLbl" for="storyAuthor">Story Author(s):</label>
+                <input type="text" class="editFormTxt storyAuthor" placeholder="Author Name"/>
+                <input type="text" class="editFormTxt storyAuthor" placeholder="Author Name"/>
+                <input type="text" class="editFormTxt storyAuthor" placeholder="Author Name"/>
+                <input type="text" class="editFormTxt storyAuthor" placeholder="Author Name"/>
+                
+                <label class="editFormLbl" for="storagePath">Upload Video:</label>
+                <input type="file" class="editFormTxt" id="storagePath"/>
+                
+                <label class="editFormLbl" for="youtubeId">YouTube ID:</label>
+                <input type="text" class="editFormTxt" id="youtubeId" v-model="selectedStory.youtube_path" placeholder="YouTube ID"/>
+                
+                <label class="editFormCheckLbl" for="featuredRotation">Featured Rotation:</label>
+                <input type="checkbox" class="editFormCheck" v-model="selectedStory.featured_rotation" id="featuredRotation" />
+
+                <input type="hidden" id="storyId" v-model="selectedStory.id"/>
+            
             </form>
                 
             <div class="attributeListContainer">
+                <h3>Select Story Tags</h3>
                 <div class="editStoryAttributeColumn">
                     <span>Subjects:</span>
-                    <ul class="selectAttributeList" id="selectSubject">
-                        <li v-for="subject in this.subjectList" v-on:click="">
-                            {{subject.name}}
+                    <ul class="selectStoryAttributeList" id="selectSubject">
+                        <li v-for="subject in this.subjectList" :id="'subject' + subject.id" v-on:click="toggleSel('subject', subject.id)">
+                            &bull; {{subject.name}}
                         </li>
                     </ul>
                 </div>
                 <div class="editStoryAttributeColumn">
                     <span>Categories:</span>
-                    <ul class="selectAttributeList" id="selectCategory">
-                        <li v-for="category in this.categoryList" v-on:click="">
-                            {{category.name}}
+                    <ul class="selectStoryAttributeList" id="selectCategory">
+                        <li v-for="category in this.categoryList" :id="'category' + category.id" v-on:click="toggleSel('category', category.id)">
+                            &bull; {{category.name}}
                         </li>
                     </ul>
                 </div>
                 <div class="editStoryAttributeColumn">
                     <span>Names:</span>
-                    <ul class="selectAttributeList" id="selectName">
-                        <li v-for="name in this.nameList" v-on:click="">
-                            {{name.name}}
+                    <ul class="selectStoryAttributeList" id="selectName">
+                        <li v-for="name in this.nameList" :id="'name' + name.id" v-on:click="toggleSel('name', name.id)">
+                            &bull; {{name.name}}
                         </li>
                     </ul>
                 </div>
                 <div class="editStoryAttributeColumn">
                     <span>Dates:</span>
-                    <ul class="selectAttributeList" id="selectPeriod">
-                        <li v-for="period in this.periodList" v-on:click="">
-                            {{period.name}}
+                    <ul class="selectStoryAttributeList" id="selectPeriod">
+                        <li v-for="period in this.periodList" :id="'period' + period.id" v-on:click="toggleSel('period', period.id)">
+                            &bull; {{period.name}}
                         </li>
                     </ul>
                 </div>
                 <div class="editStoryAttributeColumn">
                     <span>Locations:</span>
-                    <ul class="selectAttributeList" id="selectLocation">
-                        <li v-for="location in this.locationList" v-on:click="">
-                            {{location.name}}
+                    <ul class="selectStoryAttributeList" id="selectLocation">
+                        <li v-for="location in this.locationList" :id="'location' + location.id" v-on:click="toggleSel('location', location.id)">
+                            &bull; {{location.name}}
                         </li>
                     </ul>
                 </div>
@@ -109,6 +115,11 @@ export default {
       storagePath: null,
       featuredRotation: null
     },
+    selectedCategories: [],
+    selectedPeriods: [],
+    selectedLocations: [],
+    selectedSubjects: [],
+    selectedNames: []
   }),
 
   created () {
@@ -120,6 +131,7 @@ export default {
       this.getAllStories()
         .then((result) => {
           this.storyList = result
+          console.log(result)
         })
       
       this.getAllPeriods()
@@ -146,13 +158,13 @@ export default {
         .then((result) => {
           this.locationList = result
         })
-
     },
     selectStory (storyId) {
       this.getStoryById(storyId)
         .then((result) => {
+          console.log(result)
           this.selectedStory.id = result.id
-          this.selectedStory.name = result.name
+          this.selectedStory.title = result.title
           this.selectedStory.description = result.description
         })
     },
@@ -187,6 +199,9 @@ export default {
       this.selectedStory.id = null
       this.selectedStory.name = null
       this.selectedStory.description = null
+    },
+    toggleSel (type, id) {
+      $("#" + type + id).toggleClass("selectedAttribute")
     }
   }, mapActions([
     'getStoryById',
